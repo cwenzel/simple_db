37b089544aa0555170c7f1a78f27224ab1bc1ed2
cwenzel <cody.wenzel@gmail.com>
2015-11-05 08:50:40 -0700

    whitespace cleanup

--- myDB.py
+++ myDB.py
@@ -8,7 +8,7 @@ while args[0] != commandMapper.END_COMMAND:
   try:
     args = raw_input().split()
     command = commandMapper.getCommand(args[0])
-    if command is not None and  command.function is not None:
+    if command is not None and command.function is not None:
       command.function(gso, args)
   except (EOFError):
     break
