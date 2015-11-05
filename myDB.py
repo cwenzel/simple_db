ddc2fbf1858a7439ffe395d982324b4eda6b91ea
cwenzel <cody.wenzel@gmail.com>
2015-11-05 00:11:56 -0700

    a little hardening

--- myDB.py
+++ myDB.py
@@ -8,7 +8,7 @@ while args[0] != commandMapper.END_COMMAND:
   try:
     args = raw_input().split()
     command = commandMapper.getCommand(args[0])
-    if not isinstance(command, commandMapper.NoOpCommand):
+    if command is not None and  command.function is not None:
       command.function(gso, args)
   except (EOFError):
     break
